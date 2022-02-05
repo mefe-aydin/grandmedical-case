787896cad59285e9d2bc9bab7d176f617f5d83f2
mefe-aydin <m.efeaydin@outlook.com>
2022-02-05 03:51:32 +0300

    initial commit

---
+++ src/components/app.tsx
@@ -0,0 +1,7 @@
+import { FC } from "react";
+import { Home } from "../containers";
+
+// TODO redux buraya kurulacak
+export const App: FC = () => {
+  return <Home />;
+};
---
+++ src/containers/home.tsx
@@ -0,0 +1,9 @@
+import { FC } from "react";
+
+export const Home: FC = () => {
+  return (
+    <div>
+      <h1>home page</h1>
+    </div>
+  );
+};
